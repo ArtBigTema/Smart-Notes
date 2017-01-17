5f5463df91e600659a8cb4bf1cd95fa97994f6b3
Artem <belfegor732@gmail.com>
2017-01-17 09:35:01 +0400

    Добавил класс сущности Node и список.

--- app/src/main/java/av/smartnotes/activity/MainActivity.java
+++ app/src/main/java/av/smartnotes/activity/MainActivity.java
@@ -1,16 +1,36 @@
 package av.smartnotes.activity;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import av.smartnotes.R;
 import av.smartnotes.activity.activity_with.ActivityWithToolbar;
+import av.smartnotes.view.ItemsAdapter;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends ActivityWithToolbar {
+    @ViewById(R.id.rv_main)
+    protected RecyclerView recyclerView;
 
     @AfterViews
     public void afterView() {
         setToolbarTitle(R.string.app_name);
+        setRecycleView();
+    }
+
+    private void setRecycleView() {
+        if (recyclerView.getAdapter() != null) {
+            recyclerView.swapAdapter(new ItemsAdapter(), false);
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new ItemsAdapter());
+            // recyclerView.addItemDecoration(new DividerItemDecoration(5));
+        }
     }
 }
---
+++ app/src/main/java/av/smartnotes/view/ItemsAdapter.java
@@ -0,0 +1,67 @@
+package av.smartnotes.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+import av.smartnotes.R;
+import av.smartnotes.substance.Node;
+
+/**
+ * Created by Artem on 17.01.2017.
+ */
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsViewHolder> {
+    private List<Node> list = Collections.emptyList();
+    private final int LAYOUT = R.layout.rv_item;
+
+    @Override
+    public ItemsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(LAYOUT, null);
+        return new ItemsViewHolder(itemLayoutView);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemsViewHolder holder, int position) {
+        holder.bind(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class ItemsViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
+        private Node item;
+
+        public ItemsViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        public void bind(Node node) {
+            item = node;
+            TextView title = (TextView) itemView.findViewById(R.id.tv_node_title);
+            title.setText(node.getTitle());
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            return true;
+        }
+    }
+}
